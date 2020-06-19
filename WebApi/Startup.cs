9470abd3cc2e764c6a44881b33602b0646431bdb
Amr Elwakeel <amrelwakeel96@hotmail.com>
2020-06-20 00:35:52 +0200

    commend

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -46,6 +46,20 @@ namespace WebApi
 
             });
             #endregion
+
+            #region API Versioning
+            // Add API Versioning to the Project
+            services.AddApiVersioning(config =>
+            {
+                // Specify the default API Version as 1.0
+                config.DefaultApiVersion = new ApiVersion(1, 0);
+                // If the client hasn't specified the API version in the request, use the default API version number 
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                // Advertise the API versions supported for the particular endpoint
+                config.ReportApiVersions = true;
+            });
+            #endregion
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,7 +74,6 @@ namespace WebApi
 
             app.UseRouting();
 
-            #region Swagger
             // Enable middleware to serve generated Swagger as a JSON endpoint.
             app.UseSwagger();
 
@@ -70,9 +83,11 @@ namespace WebApi
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnionArchitecture");
             });
-            #endregion
 
             app.UseAuthorization();
+
+
+
 
             app.UseEndpoints(endpoints =>
             {
