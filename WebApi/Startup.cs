2f6053a6da494e38947ad0d00dc4067665a87256
Amr Elwakeel <amrelwakeel96@hotmail.com>
2020-06-20 00:09:07 +0200

    Inatial Database

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Persistence;
 
 namespace WebApi
 {
@@ -27,10 +28,12 @@ namespace WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddApplication();
 
+            services.AddPersistence(Configuration);
+
             services.AddControllers();
+
             #region Swagger
             services.AddSwaggerGen(c =>
             {
